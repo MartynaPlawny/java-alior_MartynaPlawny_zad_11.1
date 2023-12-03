2be9657ede949d5719564477ba3f04638dee185f
Martyna Plawny <kotnik909@gmail.com>
2023-12-03 12:57:21 +0100

    create project zadanie11.1

---
+++ src/main/java/com/example/javaalior_martynaplawny_zad_11_1/JavaAliorMartynaPlawnyZad111Application.java
@@ -0,0 +1,13 @@
+package com.example.javaalior_martynaplawny_zad_11_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAliorMartynaPlawnyZad111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAliorMartynaPlawnyZad111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaalior_martynaplawny_zad_11_1/JavaAliorMartynaPlawnyZad111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaalior_martynaplawny_zad_11_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAliorMartynaPlawnyZad111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
